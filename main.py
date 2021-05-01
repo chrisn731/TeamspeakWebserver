a298d0e7738118d1349a00f1f770fad27cf4b732
Chris Naporlee <chrisn731@gmail.com>
2021-05-01 00:11:15 -0400

    Make the command manager loop not hard to look at

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ SAYINGS_DIR = "C:\\Users\\jyuen\\Desktop\\Goods\\Programming\\python\\TS_Bot\\sa
 
 sayings = {}
 
-for(dirpath, dirnames, filenames) in os.walk(SAYINGS_DIR):
+for (dirpath, dirnames, filenames) in os.walk(SAYINGS_DIR):
     for filename in filenames:
         with open(SAYINGS_DIR + filename, "r") as lines:
             person = filename.split(".")[0].lower()
@@ -28,11 +28,16 @@ for(dirpath, dirnames, filenames) in os.walk(SAYINGS_DIR):
                 sayings_list.append(line)
             sayings[person] = sayings_list
 
+
+def send_global_msg(ts3conn, global_msg):
+    ts3conn.exec_("gm", msg=global_msg)
+
+
 def file_finder(ts3conn, item):
     # Create a found files
     Directory = ""
     Found = []
-    for(dirpath, dirnames, filenames) in os.walk(FILE_DIR):
+    for (dirpath, dirnames, filenames) in os.walk(FILE_DIR):
         for filename in filenames:
             if item in filename:
                 Found.append(filename)
@@ -62,7 +67,7 @@ def say(ts3conn, person):
         lines = sayings[person]
         line = lines[random.randint(0,len(lines)-1)]
         ts3conn.exec_("gm", msg=line)
-    else: 
+    else:
         ts3conn.exec_("gm", msg="Cannot find sayings for " + person)
 
 def roll_dice(ts3conn, data):
@@ -71,6 +76,10 @@ def roll_dice(ts3conn, data):
     user = data["user"]
 
     numbers = dice.split("d")
+    if numbers != 2:
+        send_global_msg(ts3conn, "Roll Usage: !roll {# of dice}d{# of sides on die}")
+        return
+
     num_die = int(numbers[0])
     die_val = int(numbers[1])
 
@@ -108,7 +117,7 @@ def roll_dice(ts3conn, data):
     ts3conn.exec_("gm", msg=output.format(user=user, dice=dice, total=total))
 
 def database_search(channelid):
-    conn = sqlite3.connect('teamspeak3-server_win64\\ts3server.sqlitedb')
+    conn = sqlite3.connect(DB_PATH)
     c = conn.cursor()
     c.execute("SELECT * FROM channel_properties;")
     channelname = "error"
@@ -122,6 +131,56 @@ def database_search(channelid):
     conn.close()
     return channelname
 
+def countdown_get(ts3conn, data):
+    timer = int(data["time"])
+    user = data["user"]
+
+    # Handle cases of funny people
+    if timer < 0:
+        ts3conn.exec_("gm", msg="Fuck you")
+        return
+
+    if timer > 1000:
+        ts3conn.exec_("gm", msg="Fuck you")
+        return
+
+    while timer != 0:
+        output = "{user}'s Timer: {timer}"
+        ts3conn.exec_("gm", msg=output.format(user=user, timer=timer))
+        time.sleep(1)
+        timer -= 1
+
+    if timer == 0:
+        output = "{user}'s Timer Is Up"
+        ts3conn.exec_("gm", msg=output.format(user=user))
+        return
+
+def start_file_finder(ts3conn, data)
+    # Find the ID of the channel so we know where to look in the database
+    file_to_find = data["file"]
+    user = data["user"]
+    channelid = file_finder(ts3conn, tokenized[1])
+
+    # ChannelID > 0, so if < 0 the file wont exit
+    if channelid == 0:
+        ts3conn.exec_("gm", msg="I'm sorry, {}, but that file does not seem \
+                        to exist.".format(user))
+    elif channelid == 4:
+        ts3conn.exec_("gm", msg="I'm sorry, {}, but you need to be more specific.\
+                        Too many results returned".format(user))
+    else:
+        # Connect to the database and look through the channel_properties
+        Channel_Name = database_search(int(channelid))
+        if Channel_Name == "error":
+            ts3conn.exec_("gm", msg="Sorry, {}, There was an issue acccessing \
+                            the server database.".format(user))
+
+        # Do something based on what the user wants
+        elif "!find" in message:
+            ts3conn.exec_("gm", msg="That file seems to be located in: {}".format(Channel_Name))
+        else:
+            pass
+
 
 def start(ts3conn):
     ts3conn.exec_("servernotifyregister", event="textserver")
@@ -131,14 +190,20 @@ def start(ts3conn):
             event = ts3conn.wait_for_event(timeout=60)
             ts3conn.send_keepalive()
         except ts3.query.TS3TimeoutError:
-            print("Timeout error, consider looking into keepalive log")
-
+            # If we get here, ts3conn.wait_for_event() stalled for the full
+            # 60 seconds. No need to do anything.
+            pass
         else:
 
             print(event[0])
 
+            # We don't want to spend time interpreting what the server is saying.
+            if (event[0]["invokerid"] == 0):
+                continue
+
             # Look at all messages sent in server chat
             message = event[0]["msg"].lower()
+
             tokenized = message.split(" ")
 
             # Get command (the first string in msg)
@@ -146,26 +211,11 @@ def start(ts3conn):
 
             # If the first token is the command !find or !get
             if "!find" == command or "!get" == command: #
-                # Find the ID of the channel so we know where to look in the database
-                channelid = file_finder(ts3conn, tokenized[1])
-
-                # ChannelID > 0, so if < 0 the file wont exit
-                if channelid == 0:
-                    ts3conn.exec_("gm", msg="I'm sorry, {}, but that file does not seem to exist.".format(event[0]["invokername"]))
-                elif channelid == 4:
-                    ts3conn.exec_("gm", msg="I'm sorry, {}, but you need to be more specific. Too many results returned".format(event[0]["invokername"]))
-                else:
-
-                # Connect to the database and look through the channel_properties
-                    Channel_Name = database_search(int(channelid))
-                    if Channel_Name == "error":
-                        ts3conn.exec_("gm", msg="Sorry, {}, There was an issue acccessing the server database.".format(event[0]["invokername"]))
-
-                # Do something based on what the user wants
-                    elif "!find" in message:
-                        ts3conn.exec_("gm", msg="That file seems to be located in: {}".format(Channel_Name))
-                    else:
-                        pass
+                data = {
+                        "file": tokenized[1],
+                        "user": event[0]["invokername"]
+                }
+                start_file_finder(ts3conn, data)
 
             elif "!roll" == command:
                 data = {
@@ -178,7 +228,12 @@ def start(ts3conn):
                 person = tokenized[1]
                 say(ts3conn, person)
 
-    return
+            elif "!cd" == command:
+                data = {
+                    "time": tokenized[1],
+                    "user": event[0]["invokername"]
+                }
+                countdown_get(ts3conn, data)
 
 if __name__ == "__main__":
     with ts3.query.TS3ServerConnection(URI) as ts3conn:
