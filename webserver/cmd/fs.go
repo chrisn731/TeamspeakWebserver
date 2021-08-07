6accbbe5c5d11ee30a6c54f669aea02705e5a53a
Chris Naporlee <chrisn731@gmail.com>
2021-08-06 23:03:54 -0400

    lol

--- webserver/cmd/fs.go
+++ webserver/cmd/fs.go
@@ -26,50 +26,51 @@ func InitServerFileMap() error {
 	}
 
 	for _, channelDir := range channelDirs {
-		if channelDir.IsDir() {
-			formPath := func(cats ...string) string {
-				base := serverFilesDirectory
-				for _, cat := range cats {
-					if base[len(base) - 1] != '/' {
-						base += "/"
-					}
-					base += cat
+		if !channelDir.IsDir() {
+			continue
+		}
+		formPath := func(cats ...string) string {
+			base := serverFilesDirectory
+			for _, cat := range cats {
+				if base[len(base) - 1] != '/' {
+					base += "/"
 				}
-				return base
+				base += cat
 			}
-			// Each file in this directory should follow the
-			// format: "channel_##" where ## is it's ID.
-			nameSplit := strings.Split(channelDir.Name(), "_")
-			if len(nameSplit) != 2 {
-				// This is only really here to make sure we do
-				// not touch the "internal" folder that resides
-				// alongside the directories we truly want
-				continue
-			}
+			return base
+		}
+		// Each file in this directory should follow the
+		// format: "channel_##" where ## is it's ID.
+		nameSplit := strings.Split(channelDir.Name(), "_")
+		if len(nameSplit) != 2 {
+			// This is only really here to make sure we do
+			// not touch the "internal" folder that resides
+			// alongside the directories we truly want
+			continue
+		}
 
-			channelFiles, err := ioutil.ReadDir(formPath(channelDir.Name()))
-			if err != nil {
-				return err
-			}
+		channelFiles, err := ioutil.ReadDir(formPath(channelDir.Name()))
+		if err != nil {
+			return err
+		}
 
-			channelID, err := strconv.ParseInt(nameSplit[1], 10, 64)
-			if err != nil {
-				// This is fatal because if we made it here then
-				// there is most likely something off with the
-				// directory and should be fixed before proceeding
-				log.Fatal(err)
-			}
+		channelID, err := strconv.ParseInt(nameSplit[1], 10, 64)
+		if err != nil {
+			// This is fatal because if we made it here then
+			// there is most likely something off with the
+			// directory and should be fixed before proceeding
+			log.Fatal(err)
+		}
 
-			// Begin adding files in this channel to the map
-			// If the data structure of the map changes, this is
-			// the ONLY part that would need to be updated.
-			for _, file := range channelFiles {
-				filePath := formPath(channelDir.Name(), file.Name())
-				ServerFiles[filePath] = FileChannel{
-					ChannelName: channelDir.Name(),
-					ChannelFilePath: formPath(channelDir.Name()),
-					ChannelID: channelID,
-				}
+		// Begin adding files in this channel to the map
+		// If the data structure of the map changes, this is
+		// the ONLY part that would need to be updated.
+		for _, file := range channelFiles {
+			filePath := formPath(channelDir.Name(), file.Name())
+			ServerFiles[filePath] = FileChannel{
+				ChannelName: channelDir.Name(),
+				ChannelFilePath: formPath(channelDir.Name()),
+				ChannelID: channelID,
 			}
 		}
 	}
